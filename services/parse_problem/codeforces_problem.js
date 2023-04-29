f57f9255b10a2874fa42b8124e9800d3c5cd35d5
siamsplash5 <siamsplash52@gmail.com>
2023-04-29 10:46:56 +0600

    upscale

--- services/parse_problem/codeforces_problem.js
+++ services/parse_problem/codeforces_problem.js
@@ -1,45 +1,43 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const { now } = require('mongoose');
 
-async function parseProblem(url) {
-    const browser = await puppeteer.launch({ headless: true });
+async function parseProblem(url, problemID) {
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url);
-    await page.waitForSelector('.problem-statement');
     const problemStatementHTML = await page.$eval('.problem-statement', (el) => el.innerHTML);
     const $ = cheerio.load(problemStatementHTML);
-    const problem = {};
+    const sampleTestHTML = $('div.sample-test').html();
 
-    problem.title = $('.header .title').text().trim();
-    problem.timeLimit = $('.header .time-limit').text().replace('time limit per test', '').trim();
-    problem.memoryLimit = $('.header .memory-limit')
-        .text()
-        .replace('memory limit per test', '')
-        .trim();
-    problem.problemStatement = {
-        bodyStatement: $('div:not([class]):not([id])').html(),
-        inputStatement: $('div.input-specification').html(),
-        outputStatement: $('div.output-specification').html(),
+    const problem = {
+        problemId: problemID,
+        title: $('.header .title').text().trim(),
+        timeLimit: $('.header .time-limit').text().replace('time limit per test', '').trim(),
+        memoryLimit: $('.header .memory-limit').text().replace('memory limit per test', '').trim(),
+        problemStatement: {
+            bodyStatement: $('div:not([class]):not([id])').html(),
+            inputStatement: $('div.input-specification').html(),
+            outputStatement: $('div.output-specification').html(),
+        },
+        notes: $('div.note').html(),
+        sampleTestCase: [
+            { input: '0', output: '5' },
+            { input: '3 7', output: '9' },
+        ],
+        tags: ['dp', 'combinatorics'],
+        rating: '1500',
+        source: url,
+        parsedAt: new Date().toLocaleString('en-US', {
+            timeZone: 'Asia/Dhaka',
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true,
+        }),
     };
-    problem.sampleTestCase = [
-        { input: '0', output: '5' },
-        { input: '3 7', output: '9' },
-    ];
-    problem.notes = $('div.note').html();
-    problem.tags = ['dp', 'combinatorics'];
-    problem.rating = '1500';
-    problem.source = url;
-    problem.parsedAt = new Date().toLocaleString('en-US', {
-        timeZone: 'Asia/Dhaka',
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: true,
-    });
     return problem;
 }
 
@@ -49,8 +47,7 @@ async function parseCodeforcesProblem(problemID) {
         const contestID = matches[1];
         const problemIndex = matches[2];
         const url = `https://codeforces.com/problemset/problem/${contestID}/${problemIndex}`;
-        const problem = await parseProblem(url);
-        problem.problemID = problemID;
+        const problem = await parseProblem(url, problemID);
 
         return problem;
     } catch (error) {
