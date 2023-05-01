35133810fbfe4a48287a2d5c13d5a1aad42a9a20
siamsplash5 <siamsplash52@gmail.com>
2023-05-01 21:38:16 +0600

    atcoder added

---
+++ database/schemas/atcoder_problem_schema.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+const problemSchema = new mongoose.Schema({
+    volume: {
+        type: Number,
+        required: true,
+    },
+    problems: [
+        {
+            problemID: {
+                type: String,
+                required: true,
+            },
+            title: {
+                type: String,
+                required: true,
+            },
+            timeLimit: {
+                type: String,
+                required: true,
+            },
+            memoryLimit: {
+                type: String,
+                required: true,
+            },
+            problemStatement: {
+                problem: {
+                    type: String,
+                    default: null,
+                },
+                constraint: {
+                    type: String,
+                    default: null,
+                },
+                input: {
+                    type: String,
+                    default: null,
+                },
+                output: {
+                    type: String,
+                    default: null,
+                },
+            },
+            sampleTestCase: {
+                inputs: [
+                    {
+                        type: String,
+                        default: null,
+                    },
+                ],
+                outputs: [
+                    {
+                        type: String,
+                        default: null,
+                    },
+                ],
+            },
+            notes: [
+                {
+                    type: String,
+                    default: null,
+                },
+            ],
+            score: {
+                type: String,
+                default: null,
+            },
+            source: {
+                type: String,
+                required: true,
+            },
+            parsedAt: Date,
+        },
+    ],
+});
+
+module.exports = problemSchema;
--- services/parse_problem/atcoder_problem.js
+++ services/parse_problem/atcoder_problem.js
@@ -1,30 +1,24 @@
 /* eslint-disable max-len */
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-// const { readProblem, createProblem } = require('../../database/queries/cf_problem_query');
+const { readProblem, createProblem } = require('../../database/queries/atcoder_problem_query');
 
-async function parseProblem(_url) {
-    const url = _url.toLowerCase();
+async function parseProblem(url, problemID) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url);
     if (page.title() === '404 Not Found - AtCoder') {
         throw new Error('Invalid Url');
     }
-    const pattern = /\/tasks\/([a-z0-9_]+)/i;
-    const match = url.match(pattern);
-    if (!(match && match.length > 1)) {
-        throw new Error('Invalid Url');
-    }
-    const problemID = match[1];
     const problemStatementHTML = await page.$eval(
         '#task-statement',
         (el) => el.parentElement.innerHTML
     );
-    // const problemStatementHTML = await page.content();
+
     const $ = cheerio.load(problemStatementHTML);
     const len = $('.part').length;
-    const title = $('span.h2').text().trim().replace('Editorial', '');
+    const title = $('span.h2').text().trim().replace('\n\t\t\tEditorial', '');
+    const score = $('span.lang-en p').find('mn').eq(0).text();
     const timeAndMemoryLimit = $('p').eq(0).text().trim();
     let timeLimit = timeAndMemoryLimit.match(/Time Limit: (\d+) sec/)[1];
     let memoryLimit = timeAndMemoryLimit.match(/Memory Limit: (\d+) MB/)[1];
@@ -32,67 +26,83 @@ async function parseProblem(_url) {
     memoryLimit = `${memoryLimit} megabytes`;
     const inputs = [];
     const outputs = [];
+    const notes = [];
     const totalPreTag = len / 2 - 4;
-    console.log(totalPreTag);
     for (let i = totalPreTag; i <= 2 * totalPreTag - 1; i += 1) {
         const data = $(`#pre-sample${i}`).text().trim();
         if (i % 2 === 0) {
             inputs.push(data);
         } else {
             outputs.push(data);
+            const notesInnerHtml = $(`#pre-sample${i}`)
+                .nextAll()
+                .map((idx, el) => $.html(el))
+                .get()
+
+                .join('');
+            if (notesInnerHtml !== null) {
+                notes.push(notesInnerHtml);
+            }
         }
     }
 
-    // const problem = {
-    //     problemID,
-    //     title,
-    //     timeLimit,
-    //     memoryLimit,
-    //     problemStatement: {
-    //         problem: $('div.part')
-    //             .eq(len + 1)
-    //             .html(),
-    //         constraint: $('div.part')
-    //             .eq(len + 2)
-    //             .html(),
-    //         input: $('div.part')
-    //             .eq(len + 3)
-    //             .html(),
-    //         output: $('div.part')
-    //             .eq(len + 4)
-    //             .html(),
-    //     },
-    //     sampleTestCase: {
-    //         sampleInputs,
-    //         sampleOutputs,
-    //     },
-    //     notes: $('div.note').html(),
-    //     score,
-    //     source: url,
-    //     parsedAt: new Date().toLocaleString('en-US', {
-    //         timeZone: 'Asia/Dhaka',
-    //         day: '2-digit',
-    //         month: '2-digit',
-    //         year: 'numeric',
-    //         hour: 'numeric',
-    //         minute: 'numeric',
-    //         second: 'numeric',
-    //         hour12: true,
-    //     }),
-    // };
-    return problemStatementHTML;
+    const problem = {
+        problemID,
+        title,
+        timeLimit,
+        memoryLimit,
+        problemStatement: {
+            problem: $('div.part')
+                .eq(len / 2)
+                .html(),
+            constraint: $('div.part')
+                .eq(len / 2 + 1)
+                .html(),
+            input: $('div.part')
+                .eq(len / 2 + 2)
+                .html(),
+            output: $('div.part')
+                .eq(len / 2 + 3)
+                .html(),
+        },
+        sampleTestCase: {
+            inputs,
+            outputs,
+        },
+        notes,
+        score,
+        source: url,
+        parsedAt: new Date().toLocaleString('en-US', {
+            timeZone: 'Asia/Dhaka',
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true,
+        }),
+    };
+    return problem;
 }
 
-async function parseAtcoderProblem(url) {
+async function parseAtcoderProblem(_url) {
     try {
-        // let problem = await readProblem(problemID);
-        // if (problem === 'not found') {
-        //     problem = await parseProblem(problemID);
-        //     await createProblem(problem);
-        // }
-        // return problem;
-        const problem = await parseProblem(url);
+        const url = _url.toLowerCase();
+        const pattern = /\/tasks\/([a-z0-9_]+)/i;
+        const match = url.match(pattern);
+        if (!(match && match.length > 1)) {
+            throw new Error('Invalid Url');
+        }
+        const problemID = match[1];
+        let problem = await readProblem(url, problemID);
+        if (problem === 'not found') {
+            problem = await parseProblem(url, problemID);
+            await createProblem(problem);
+        }
         return problem;
+        // const problem = await parseProblem(url);
+        // return problem;
     } catch (error) {
         console.error(error);
         throw new Error(error);
