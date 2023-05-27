75857486405c5b59aafd525d6f1aa93c9b0a32a6
siamsplash5 <siamsplash52@gmail.com>
2023-05-28 03:49:07 +0600

    fix: update the status code to 400 for response handlers, avoiding axios conflict in the front-end

--- handlers/response.handler.js
+++ handlers/response.handler.js
@@ -1,11 +1,11 @@
 const responseWithData = (res, statusCode, data) => res.status(statusCode).json(data);
 
-const error = (res) => responseWithData(res, 500, {
+const error = (res) => responseWithData(res, 200, {
         status: 500,
         message: 'Opps! Something went wrong!',
     });
     
-const badRequest = (res, message) => responseWithData(res, 400, {
+const badRequest = (res, message) => responseWithData(res, 200, {
         status: 400,
         message,
     });
@@ -16,12 +16,12 @@ const created = (res, data) => responseWithData(res, 201, data);
 
 const pending = (res, data) => responseWithData(res, 202, data);
 
-const unauthorize = (res, message) => responseWithData(res, 401, {
+const unauthorize = (res, message) => responseWithData(res, 200, {
         status: 401,
         message
     });
 
-const notfound = (res) => responseWithData(res, 404, {
+const notfound = (res) => responseWithData(res, 200, {
         status: 404,
         message: 'Resource not found',
     });
