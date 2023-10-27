1aa26b259ad1dbaa8b949d1fb782221e6de77908
siamsplash5 <siamsplash52@gmail.com>
2023-10-27 10:01:40 +0600

    Added docker file and re-configured the puppeteer

--- services/parse_problem/atcoder_problem.js
+++ services/parse_problem/atcoder_problem.js
@@ -23,6 +23,11 @@ async function parseProblem(url, judge, problemID) {
     const browser = await puppeteer.launch({
         // executablePath,
         // args: edgeChromium.args,
+        args: ['--disable-setuid-sandbox', '--no-sandbox', '--single-process', '--no-zygote'],
+        executablePath:
+            process.env.NODE_ENV === 'production'
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
         headless: true,
     });
 
