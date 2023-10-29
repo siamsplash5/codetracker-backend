fa65f02b879ce1edf9fbf12acdde442e802035bd
siamsplash5 <siamsplash52@gmail.com>
2023-10-29 20:52:32 +0600

    fix: response

--- routers/checkRouter.js
+++ routers/checkRouter.js
@@ -6,6 +6,7 @@ const checkRouter = express.Router();
 checkRouter.get('/uptime-robot', async (req, res) => {
     try {
         console.log('UptimeRobot');
+        responseHandler.ok('Server is up');
     } catch (error) {
         console.log(error);
         responseHandler.error(res);
